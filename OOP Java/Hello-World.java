90c99628286eefedf84ff97d65529291afd80076
Hafizh Hilman Asyhari <81144031+hafizhhasyhari@users.noreply.github.com>
2024-11-25 10:35:34 +0700

    Create Hello-World.java

---
+++ OOP Java/Hello-World.java
@@ -0,0 +1,11 @@
+package [hello_dunia;
+// *Author Hafizh
+public class PHello_Dunia {
+  /**
+   * @param args the command line arguments
+   */
+public static void main(String[] args) {
+  // 1000 code application logic here
+  System.out.println("Hello Dunia");
+  }
+}
